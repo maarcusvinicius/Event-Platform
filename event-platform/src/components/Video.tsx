03c078670791bad62ff99ce338a355649f3979c3
maarcusvinicius <marcus.editor77@gmail.com>
2022-10-09 15:16:45 -0300

    adjuste video

--- event-platform/src/components/Video.tsx
+++ event-platform/src/components/Video.tsx
@@ -27,7 +27,7 @@ export function Video(props: VideoProps) {
   return (
     <div className="flex-1">
       <div className="bg-black flex justify-center">
-        <div className="h-full w-full max-w-[1100px] max-h-[60vh] aspect-video">
+        <div className="h-full w-full max-h-[60vh] aspect-video">
           <Player>
             <Youtube videoId={data.lesson.videoId} />
             <DefaultUi />
